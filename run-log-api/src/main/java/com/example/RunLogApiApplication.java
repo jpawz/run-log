047eff9137466ff534b1752974cfbff0ff1b6823
Paweł J <jpawz@outlook.com>
2022-06-24 19:48:00 +0200

    Spring boot project initialized.

---
+++ run-log-api/src/main/java/com/example/RunLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunLogApiApplication.class, args);
+	}
+
+}
