2af6c6e74dc0d85a8159a2c539195ab208d3e660
Paweł J <jpawz@outlook.com>
2022-06-24 23:24:15 +0200

    UI initial commit.

---
+++ run-log-ui/src/app/components/run-log-list/run-log-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-run-log-list',
+  templateUrl: './run-log-list.component.html',
+  styleUrls: ['./run-log-list.component.css']
+})
+export class RunLogListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
