b6cbea0e892a8215d333bb7a13f0bc6782d3ed87
Paweł J <jpawz@outlook.com>
2022-07-02 20:05:41 +0200

    Add tests

--- run-log-api/src/test/java/com/example/controller/TrainingControllerTest.java
+++ run-log-api/src/test/java/com/example/controller/TrainingControllerTest.java
@@ -2,13 +2,18 @@ package com.example.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import com.example.domain.TrainingDTO;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TrainingControllerTest {
@@ -35,6 +40,18 @@ class TrainingControllerTest {
     @Test
     void shouldOpenAddNewTrainingPage() {
 	ResponseEntity<String> response = template.getForEntity(url + "add", String.class);
-	assertThat(response.getBody()).contains("Date:");
+	assertThat(response.getBody()).contains("Add new training");
+    }
+
+    @Test
+    void shouldAddNewTraining() {
+	TrainingDTO trainingDTO = new TrainingDTO();
+	trainingDTO.setDate(LocalDate.of(2022, 7, 1));
+	trainingDTO.setDistanceInKilometer(5);
+	trainingDTO.setDurationM(30);
+
+	ResponseEntity<String> response = template.postForEntity(url + "add", trainingDTO, String.class);
+
+	assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
