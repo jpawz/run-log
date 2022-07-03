084d5848f46213f0cc962cab215ff6241ccfcc59
Paweł J <jpawz@outlook.com>
2022-07-03 19:40:51 +0200

    Update procfile, rename app

--- src/main/java/com/example/RunLogApplication.java
+++ src/main/java/com/example/RunLogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RunLogApiApplication {
+public class RunLogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunLogApiApplication.class, args);
+		SpringApplication.run(RunLogApplication.class, args);
 	}
 
 }
