3d49ad50c58aa0d5fc2cfc448a4b6909966f7a32
Paweł J <jpawz@outlook.com>
2022-07-04 18:39:50 +0200

    Switched from TestRestTemplate to MockMvc

--- src/test/java/com/example/controller/TrainingControllerTest.java
+++ src/test/java/com/example/controller/TrainingControllerTest.java
@@ -15,10 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.domain.Training;
@@ -39,9 +37,6 @@ class TrainingControllerTest {
     @Autowired
     private TrainingRepository repository;
 
-    @Autowired
-    private TestRestTemplate template;
-
     @BeforeEach
     public void setUp() throws Exception {
 	repository.deleteAll();
@@ -49,15 +44,15 @@ class TrainingControllerTest {
     }
 
     @Test
-    void shouldOpenStartPage() {
-	ResponseEntity<String> response = template.getForEntity(url, String.class);
-	assertThat(response.getBody()).contains("Trainings");
+    void shouldOpenStartPage() throws Exception {
+	mockMvc.perform(get(url))
+		.andExpect(content().string(containsString("Training")));
     }
 
     @Test
-    void shouldOpenAddNewTrainingPage() {
-	ResponseEntity<String> response = template.getForEntity(url + "add", String.class);
-	assertThat(response.getBody()).contains("Add new training");
+    void shouldOpenAddNewTrainingPage() throws Exception {
+	mockMvc.perform(get(url + "add"))
+		.andExpect(content().string(containsString("Add new training")));
     }
 
     @Test
