196b1702a320379bdd72f349c0cd8a7d87ae9230
Paweł J <jpawz@outlook.com>
2022-07-05 18:15:07 +0200

    Remove service Removed because it only reflected repository

--- src/main/java/com/example/controller/TrainingController.java
+++ src/main/java/com/example/controller/TrainingController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.domain.TrainingDTO;
 import com.example.mapper.Mapper;
-import com.example.service.TrainingService;
+import com.example.repository.TrainingRepository;
 
 @Controller
 public class TrainingController {
@@ -23,14 +24,14 @@ public class TrainingController {
     private static final String REDIRECT_TO_HOMEPAGE = "redirect:/index";
 
     @Autowired
-    private TrainingService service;
+    private TrainingRepository repository;
 
     @Autowired
     private Mapper mapper;
 
     @GetMapping(value = { "/", "/index" })
     public String index(Model model) {
-	List<TrainingDTO> trainings = StreamSupport.stream(service.getAllTrainings().spliterator(), false)
+	List<TrainingDTO> trainings = StreamSupport.stream(repository.findAll().spliterator(), false)
 		.map(mapper::toDto)
 		.toList();
 	model.addAttribute("trainings", trainings);
@@ -50,13 +51,13 @@ public class TrainingController {
 	    return "add";
 	}
 
-	service.saveTraining(mapper.toTraining(trainingDTO));
+	repository.save(mapper.toTraining(trainingDTO));
 	return REDIRECT_TO_HOMEPAGE;
     }
 
     @GetMapping("/edit/{id}")
     public String updateTrainingForm(@PathVariable("id") int id, Model model) {
-	TrainingDTO trainingdto = mapper.toDto(service.getTraining(id));
+	TrainingDTO trainingdto = mapper.toDto(repository.findById(id).orElseThrow(EntityNotFoundException::new));
 	model.addAttribute("trainingDTO", trainingdto);
 	return "edit";
     }
@@ -69,13 +70,13 @@ public class TrainingController {
 	    return "edit";
 	}
 
-	service.saveTraining(mapper.toTraining(trainingDTO));
+	repository.save(mapper.toTraining(trainingDTO));
 	return REDIRECT_TO_HOMEPAGE;
     }
 
     @GetMapping("/delete/{id}")
     public String deleteTraining(@PathVariable("id") int id, Model model) {
-	service.deleteTrainingById(id);
+	repository.deleteById(id);
 	return REDIRECT_TO_HOMEPAGE;
     }
 }
